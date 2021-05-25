ff328c785f5b90595cdecd3c7581c3dd70900b9c
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-25 10:48:45 +0200

    add include to header.php :arrow_right: index.php, create tag header, h1, and list :arrow_right: components/header.php

---
+++ components/header.php
@@ -0,0 +1,11 @@
+<header>
+    <h1>Systaire</h1>
+
+    <ul>
+        <li><a href="#">PlacerHolder</a></li>
+        <li><a href="#">PlacerHolder</a></li>
+        <li><a href="#">PlacerHolder</a></li>
+        <li><a href="#">PlacerHolder</a></li>
+        <li><a href="#">PlacerHolder</a></li>
+    </ul>
+</header>
