ab9e989a8efead9a2bff7ec1fdce169554363653
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-25 10:53:44 +0200

    add link to login_createAccount && create_articles.php :arrow_right: components/header.php

--- components/header.php
+++ components/header.php
@@ -5,7 +5,7 @@
         <li><a href="#">PlacerHolder</a></li>
         <li><a href="#">PlacerHolder</a></li>
         <li><a href="#">PlacerHolder</a></li>
-        <li><a href="#">PlacerHolder</a></li>
-        <li><a href="#">PlacerHolder</a></li>
+        <li><a href="http://localhost:8888/test/Systaire/layouts/create_articles.php">PlacerHolder</a></li>
+        <li><a href="http://localhost:8888/test/Systaire/layouts/login_createAccount.php">Login / create account</a></li>
     </ul>
 </header>
