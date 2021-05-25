3bfbafddc0ff24c1f04737fe7444a23e188b661a
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-25 21:12:26 +0200

    add link in the first title : redirect to index.php, replace 1 placeHolder by create articles :arrow_right: components/header.php

--- components/header.php
+++ components/header.php
@@ -1,11 +1,11 @@
 <header>
-    <h1>Systaire</h1>
+    <h1><a href="http://localhost:8888/test/Systaire/">Systaire</a></h1>
 
     <ul>
         <li><a href="#">PlacerHolder</a></li>
         <li><a href="#">PlacerHolder</a></li>
         <li><a href="#">PlacerHolder</a></li>
-        <li><a href="http://localhost:8888/test/Systaire/layouts/create_articles.php">PlacerHolder</a></li>
+        <li><a href="http://localhost:8888/test/Systaire/layouts/create_articles.php">create articles</a></li>
         <li><a href="http://localhost:8888/test/Systaire/layouts/login_createAccount.php">Login / create account</a></li>
     </ul>
 </header>
