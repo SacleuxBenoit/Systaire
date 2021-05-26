f2c3099ce7ccbdf022582f6b2d63b17884acd1be
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-26 18:09:25 +0200

    create the form :arrow_right: create_articles.php

--- layouts/create_articles.php
+++ layouts/create_articles.php
@@ -11,5 +11,28 @@
     <?php
         include('../components/header.php');
     ?>
+
+    <div>
+        <form action="#" method="post">
+            <p>
+                <label for="title">Titre :</label>
+                <input type="text" name="title" id="title">
+            </p>
+
+            <p>
+                <label for="content">Content :</label>
+                <input type="text" name="content" id="content">
+            </p>
+
+            <p>
+                <label for="description">description :</label>
+                <input type="text" name="description" id="description">
+            </p>
+
+            <p>
+                <input type="submit" value="Submit">
+            </p>
+        </form>
+    </div>
 </body>
 </html>
