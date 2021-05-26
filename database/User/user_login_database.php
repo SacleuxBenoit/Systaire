b136394686bc1659c8dd549c589d933d44649a18
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-26 14:00:14 +0200

    user can now use the part (login) :arrow_right: user_login_database.php

---
+++ database/User/user_login_database.php
@@ -0,0 +1,21 @@
+<?php
+ session_start();
+ include('../../login_database.php');
+ include('../connection_database.php');
+
+ $user_login = $bdd->prepare('SELECT * FROM user WHERE username = :username');
+ $user_login->bindParam(':username', $_POST['LoginPseudo']);
+ $user_login->execute();
+ $donnees = $user_login->fetch();
+ 
+ if(isset($_POST['LoginPseudo'])){
+    if($_POST['LoginPseudo'] == $donnees['username'] && password_verify($_POST['LoginPass'],$donnees['pass'])){
+        $_SESSION['username'] = $_POST['LoginPseudo'];
+        $_SESSION['id_user'] = $donnees['id'];
+        header('Location: ../../index.php');
+    }else{
+        header('Location: ../../index.php');
+    }
+ }
+
+ ?> 
