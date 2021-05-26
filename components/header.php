24b05e0fe6d761be1a232ecdf768238198b6d6a6
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-26 22:47:50 +0200

    if  username exist, display logout, else display login :arrow_right: components/header.php, add session_start() :arrow_right: index.php

--- components/header.php
+++ components/header.php
@@ -6,6 +6,16 @@
         <li><a href="#">PlacerHolder</a></li>
         <li><a href="#">PlacerHolder</a></li>
         <li><a href="http://localhost:8888/test/Systaire/layouts/create_articles.php">create articles</a></li>
+        <?php
+            if(empty($_SESSION['username'])){
+        ?>
         <li><a href="http://localhost:8888/test/Systaire/layouts/login_createAccount.php">Login / create account</a></li>
+        <?php
+            }else{
+        ?>
+            <li><a href="http://localhost:8888/test/Systaire/database/User/user_logout_database.php">Logout</a></li>
+        <?php
+            }
+        ?>
     </ul>
 </header>
