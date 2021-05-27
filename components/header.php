b3e33f71adf7761385735f494835195ab4e0e734
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-27 18:24:16 +0200

    update the list :arrow_right: components/header.php

--- components/header.php
+++ components/header.php
@@ -2,10 +2,10 @@
     <h1><a href="http://localhost:8888/test/Systaire/">Systaire</a></h1>
 
     <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Top lectures</a></li>
         <li><a href="#">PlacerHolder</a></li>
-        <li><a href="#">PlacerHolder</a></li>
-        <li><a href="#">PlacerHolder</a></li>
-        <li><a href="http://localhost:8888/test/Systaire/layouts/create_articles.php">create articles</a></li>
+        <li><a href="http://localhost:8888/test/Systaire/layouts/create_articles.php">Create articles</a></li>
         <?php
             if(empty($_SESSION['username'])){
         ?>
