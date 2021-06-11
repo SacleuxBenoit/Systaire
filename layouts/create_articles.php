dd15eeb96e98f790f04592dcc61c4f263d03dedd
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-11 16:22:05 +0200

    add css link for components_header.css :arrow_right: article.php && create_articles.php

--- layouts/create_articles.php
+++ layouts/create_articles.php
@@ -10,6 +10,7 @@ if(!isset($_SESSION['username'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/style_components/components_header.css">
     <link rel="stylesheet" href="../css/style_create_articles.css">
     <title>Document</title>
 </head>
