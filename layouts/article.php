00469e04f1e0cf0a1d2a686988ef7fd316ec3f00
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-16 19:28:30 +0200

    display grid + add margin left && right :arrow_right: style_article.css, add css link :arrow_right: layouts/article.php

--- layouts/article.php
+++ layouts/article.php
@@ -9,6 +9,7 @@ include('../database/connection_database.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style_components/components_header.css">
+    <link rel="stylesheet" href="../css/style_article.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
