1ba7b36cb449124125d5b81e64fad6976ed1fdf7
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-17 23:24:14 +0200

    if the user is connected display the li create articles :arrow_right: components/header.php

--- components/header.php
+++ components/header.php
@@ -13,15 +13,13 @@
                     <li><a href="http://localhost:8888/test/Systaire/layouts/category.php?categorie=<?php echo $fetch_categorie['categorie']?>"><?php echo $fetch_categorie['categorie']?></a></li>
                 <?php
             }
-        ?>
-        <li><a href="http://localhost:8888/test/Systaire/layouts/create_articles.php">Create articles</a></li>
-        <?php
             if(empty($_SESSION['username'])){
         ?>
         <li><a href="http://localhost:8888/test/Systaire/layouts/login_createAccount.php">Login / create account</a></li>
         <?php
             }else{
         ?>
+            <li><a href="http://localhost:8888/test/Systaire/layouts/create_articles.php">Create articles</a></li>
             <li><a href="http://localhost:8888/test/Systaire/database/User/user_logout_database.php">Logout</a></li>
         <?php
             }
