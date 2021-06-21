7532aab44ecc2d5a5104c714fa4df6b24aee5256
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-21 15:44:12 +0200

    add link to css file, correct misspelling with title + include to component header.php :arrow_right: modify_article.php

--- layouts/modify_article.php
+++ layouts/modify_article.php
@@ -9,9 +9,12 @@ include('../database/connection_database.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Systiare - MODIFY</title>
+    <link rel="stylesheet" href="../css/style_components/components_header.css">
+    <title>Systaire - MODIFY</title>
 </head>
 <body>
-    
+    <?php
+        include('../components/header.php');
+    ?>
 </body>
 </html>
