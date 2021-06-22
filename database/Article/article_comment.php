f46bca1b2c81e6c476a2513f42eae98017f00570
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-22 14:23:05 +0200

    update database with createdAT :arrow_right: article_comment.php

--- database/Article/article_comment.php
+++ database/Article/article_comment.php
@@ -14,7 +14,7 @@ $get_id_user = $select_user->fetch();
 // insert comment to database
 
 if(!empty($_POST['comment'])){
-    $insert_comment = $bdd->prepare('INSERT INTO comment(id_user,id_article,comment) VALUES(:id_user,:id_article,:comment)');
+    $insert_comment = $bdd->prepare('INSERT INTO comment(id_user,id_article,comment,createdAT) VALUES(:id_user,:id_article,:comment,NOW())');
     $insert_comment->bindParam(':id_user',$get_id_user['id_user']);
     $insert_comment->bindParam(':id_article', $_SESSION['id_article']);
     $insert_comment->bindParam(':comment', $_POST['comment']);
