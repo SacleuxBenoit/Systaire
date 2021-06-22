39288c4f6eb1dee59cbee4f22337bf092c561128
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-22 14:56:32 +0200

    :memo: hotfixe : delete comment with their articles :arrow_right: article_delete_database.php

--- database/Article/article_delete_database.php
+++ database/Article/article_delete_database.php
@@ -7,6 +7,10 @@ if(isset($_SESSION['id_article'])){
     $delete_article = $bdd->prepare('DELETE FROM article WHERE id_article = :id_article');
     $delete_article->bindParam(':id_article', $_SESSION['id_article']);
     $delete_article->execute();
+
+    $delete_comment = $bdd->prepare('DELETE FROM comment WHERE id_article = :id_article');
+    $delete_comment->bindParam(':id_article', $_SESSION['id_article']);
+    $delete_comment->execute();
     header('Location: ../../index.php');
 }else{
     header('Location: ../../index.php');
