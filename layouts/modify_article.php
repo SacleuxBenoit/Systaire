cb91026cb01d4913386a020d5a1e694fd8023393
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-23 12:00:51 +0200

    update with form : input (title,content,description,categorie) :arrow_right: modify_article.php

--- layouts/modify_article.php
+++ layouts/modify_article.php
@@ -10,11 +10,53 @@ include('../database/connection_database.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/style_components/components_header.css">
+    <link rel="stylesheet" href="../css/style_create_articles.css">
     <title>Systaire - MODIFY</title>
 </head>
 <body>
     <?php
         include('../components/header.php');
     ?>
+
+    <div>
+        <form action="../database/Article/article_modify_database.php" method="post">
+            <p>
+                <label for="modifyTitle">Titre :</label>
+                <input type="text" name="modifyTitle" id="modifyTitle">
+            </p>
+
+            <p>
+                <label for="modifyContent">Content :</label>
+                <input type="text" name="modifyContent" id="modifyContent">
+            </p>
+
+            <p>
+                <label for="modifySmallDesc">description :</label>
+                <input type="text" name="modifySmallDesc" id="modifySmallDesc">
+            </p>
+
+            <p>
+            <select name="modifyCategorie" id="modifyCategorie">
+            <label for="modifyCategorie">categorie : </label>
+            <?php
+
+                $select_category = $bdd->query('SELECT categorie FROM categorie');
+                $select_category->execute();
+
+                while($display_category = $select_category->fetch()){
+                    ?>
+                        <option value="<?php echo $display_category['categorie']?>"><?php echo $display_category['categorie']?></option>
+                    <?php
+                }
+            ?>
+            </select>
+
+            </p>
+
+            <p>
+                <input type="submit" value="Submit">
+            </p>
+        </form>
+    </div>
 </body>
 </html>
