e3af0d6aa29e65d5b2e5aa65b7e6bf35eb0bae9c
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-23 14:18:54 +0200

    add include to connection_database, login_database, component header, session_start + create form with 2 input : 1 type text for change username and the other type submit :arrow_right: layouts/settings.php

--- layouts/settings.php
+++ layouts/settings.php
@@ -1,12 +1,30 @@
+<?php
+session_start();
+include('../login_database.php');
+include('../database/connection_database.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style_components/components_header.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Systaire - SETTINGS</title>
 </head>
 <body>
-    
+    <?php
+        include('../components/header.php');
+    ?>
+    <form action="../database/User/user_settings_database.php" method="post">
+        <p>
+            <label for="changeUsername">change username :</label>
+            <input type="text" name="changeUsername" id="changeUsername">
+        </p>
+
+        <p>
+            <input type="submit" value="Submit">
+        </p>
+    </form>
 </body>
 </html>
