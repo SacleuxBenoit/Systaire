29887424f1b995a378b0be14f26128ac8dd4bc00
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-23 13:24:49 +0200

    admin can now update an article :arrow_right: article_modify_database.php

---
+++ database/Article/article_modify_database.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+include('../../login_database.php');
+include('../connection_database.php');
+
+$update_article = $bdd->prepare('UPDATE article SET title = :modifyTitle, smallDesc =:modifySmallDesc, content =:modifyContent, categorie =:modifyCategorie WHERE title =:title');
+$update_article->bindParam(':modifyTitle', $_POST['modifyTitle']);
+$update_article->bindParam(':modifySmallDesc', $_POST['modifySmallDesc']);
+$update_article->bindParam(':modifyContent', $_POST['modifyContent']);
+$update_article->bindParam(':modifyCategorie', $_POST['modifyCategorie']);
+$update_article->bindParam(':title', $_SESSION['title']);
+$update_article->execute();
+header('Location: ../../index.php');
+?>
