413ac1e4345e425aba6969d49cf08037457b8d9a
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-25 15:34:00 +0200

    merge the part body :arrow_right: style_category.css, style_index.css in file :arrow_right: style_body.css, and add link css to :arrow_right: index.php && category.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ include('./database/connection_database.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style_body.css">
     <link rel="stylesheet" href="./css/style_components/components_header.css">
     <link rel="stylesheet" href="./css/style_index.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
