ded65eedea4d69062e3092b944c2ff5a170443e8
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-26 18:24:43 +0200

    verify if someone already have the username :arrow_right: user_register_database.php

--- database/User/user_register_database.php
+++ database/User/user_register_database.php
@@ -3,19 +3,19 @@ session_start();
 include('../../login_database.php');
 include('../connection_database.php');
 
-$_SESSION['username'] = $_POST['RegisterPseudo'];
+$find_username = $bdd->prepare('SELECT username FROM user WHERE username = :username');
+$find_username->bindParam(':username', $_POST['RegisterPseudo']);
+$find_username->execute();
+$verify_username = $find_username->fetch();
 
-if(!empty($_POST['RegisterPseudo']) && !empty($_POST['RegisterPass'])){
+if(!$verify_username && !empty($_POST['RegisterPseudo']) && !empty($_POST['RegisterPass']) && $search_user['username'] !== $_POST['RegisterPseudo']){
     $pass_hash = password_hash($_POST['RegisterPass'], PASSWORD_DEFAULT);
     $user_info = $bdd->prepare('INSERT INTO user(username,pass) VALUES(:RegisterPseudo, :pass)');
     $user_info->bindParam(':RegisterPseudo', $_POST['RegisterPseudo']);
     $user_info->bindParam(':pass', $pass_hash);
     $user_info->execute();
     
-    $user_id = $bdd->prepare('SELECT id_user FROM user WHERE username = :username');
-    $user_id->bindParam(':username', $_POST['RegisterPseudo']);
-    $user_id->execute();
-
+    $_SESSION['username'] = $_POST['RegisterPseudo'];
     header('Location: ../../index.php');
 }else
 {
