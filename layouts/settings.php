0f18de46c532a60684754f14e05cccd2bcb5b718
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-26 14:47:15 +0200

    verify if SettingsPseudo exist with isset() :arrow_right: layouts/settings.php

--- layouts/settings.php
+++ layouts/settings.php
@@ -2,7 +2,9 @@
 session_start();
 include('../login_database.php');
 include('../database/connection_database.php');
-$_SESSION['username'] = $_POST['SettingsPseudo'];
+if(isset($_POST['SettingsPseudo'])){
+    $_SESSION['username'] = $_POST['SettingsPseudo'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,25 +20,27 @@ $_SESSION['username'] = $_POST['SettingsPseudo'];
     <?php
         include('../components/header.php');
     ?>
-    <form action="../database/Settings/settings_rename_database.php" method="post">
-        <p>
-            <label for="changeUsername">change username :</label>
-            <input type="text" name="changeUsername" id="changeUsername">
-            <input type="submit" value="Submit">
-        </p>
-    </form>
+    <div class="container">
+        <form action="../database/Settings/settings_rename_database.php" method="post">
+            <p>
+                <label for="changeUsername">change username :</label>
+                <input type="text" name="changeUsername" id="changeUsername">
+                <input type="submit" value="Submit">
+            </p>
+        </form>
 
-    <form action="../database/Settings/settings_changePassword.php" method="post">
-        <p>
-            <label for="changePassword">change password :</label>
-            <input type="text" name="changePassword" id="changePassword">
-        </p>
+        <form action="../database/Settings/settings_changePassword.php" method="post">
+            <p>
+                <label for="changePassword">change password :</label>
+                <input type="text" name="changePassword" id="changePassword">
+            </p>
 
-        <p>
-            <label for="confirmPassword">confirm password :</label>
-            <input type="text" name="confirmPassword" id="confirmPassword">
-            <input type="submit" value="Submit">
-        </p>
-    </form>
+            <p>
+                <label for="confirmPassword">confirm password :</label>
+                <input type="text" name="confirmPassword" id="confirmPassword">
+                <input type="submit" value="Submit">
+            </p>
+        </form>
+    </div>
 </body>
 </html>
