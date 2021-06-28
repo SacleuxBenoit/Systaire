fdf86bf0b5998bf485b7431c022c8a8b115069d9
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-28 15:27:10 +0200

    user can now delete an account :arrow_right: settings_deleteAccount_database.php

--- database/Settings/settings_deleteAccount_database.php
+++ database/Settings/settings_deleteAccount_database.php
@@ -2,4 +2,9 @@
 session_start();
 include('../../login_database.php');
 include('../connection_database.php');
+
+$delete_account = $bdd->prepare('DELETE FROM user WHERE username =:username');
+$delete_account->bindParam(':username', $_SESSION['username']);
+$delete_account->execute();
+header('location: ../../index.php');
 ?>
