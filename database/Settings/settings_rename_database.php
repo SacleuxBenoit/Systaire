466f4152ecb13951135f9316c7e37da258be9fe2
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-29 15:09:26 +0200

    :memo: hotfixe : user can't take an username that is already taken :arrow_right: settings_rename_database.php

--- database/Settings/settings_rename_database.php
+++ database/Settings/settings_rename_database.php
@@ -3,9 +3,20 @@ session_start();
 include('../../login_database.php');
 include('../connection_database.php');
 
-$change_username = $bdd->prepare('UPDATE user SET username = :newUsername WHERE username = :username');
-$change_username->bindParam(':newUsername', $_POST['changeUsername']);
-$change_username->bindParam(':username', $_SESSION['username']);
-$change_username->execute();
-header('Location: ../../index.php');
+$find_user = $bdd->prepare('SELECT username FROM user WHERE username =:username');
+$find_user->bindParam(':username', $_POST['changeUsername']);
+$find_user->execute();
+
+$user_found = $find_user->fetch();
+
+if(!$user_found){
+    $change_username = $bdd->prepare('UPDATE user SET username = :newUsername WHERE username = :username');
+    $change_username->bindParam(':newUsername', $_POST['changeUsername']);
+    $change_username->bindParam(':username', $_SESSION['username']);
+    $change_username->execute();
+    header('Location: ../../index.php');
+}else{
+    header('Location: ../../layouts/settings.php');
+}
+
 ?>
