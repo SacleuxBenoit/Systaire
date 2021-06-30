9a59b4f1fe0cebc332277297f1268b73b918ee0a
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-30 20:16:18 +0200

    add the possibility to delete a comment :arrow_right: Comment/delete_comment.php

--- database/Comment/delete_comment.php
+++ database/Comment/delete_comment.php
@@ -2,4 +2,14 @@
 session_start();
 include('../../login_database.php');
 include('../connection_database.php');
+
+if(isset($_GET['id_comment'])){
+    $delete_comment = $bdd->prepare('DELETE FROM comment WHERE id_comment =:id_comment');
+    $delete_comment->bindParam('id_comment', $_GET['id_comment']);
+    $delete_comment->execute();
+    header('Location: ../../index.php');
+}else{
+    header('Location: ../../index.php');
+}
+
 ?>
