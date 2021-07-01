69aa7499f968e0feefb64df6f1d4368f5d874450
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-07-01 18:42:06 +0200

    user can now change his password :arrow_right: settings_changePassword.php

--- database/Settings/settings_changePassword.php
+++ database/Settings/settings_changePassword.php
@@ -2,4 +2,13 @@
 session_start();
 include('../../login_database.php');
 include('../connection_database.php');
+
+$pass_hash = password_hash($_POST['changePassword'], PASSWORD_DEFAULT);
+
+if(!empty($_POST['changePassword']))
+$change_password = $bdd->prepare('UPDATE user SET pass =:pass WHERE username =:username');
+$change_password->bindParam(':pass', $pass_hash);
+$change_password->bindParam(':username', $_SESSION['username']);
+$change_password->execute();
+header('Location: ../../layouts/settings.php');
 ?>
