f0ad374c7b697c9bdd25f59dcfc3ecf5029f35ce
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-07-01 18:44:04 +0200

    :memo: verify if confirm password == changePassword :arrow_right: settings_changePassword.php

--- database/Settings/settings_changePassword.php
+++ database/Settings/settings_changePassword.php
@@ -5,10 +5,14 @@ include('../connection_database.php');
 
 $pass_hash = password_hash($_POST['changePassword'], PASSWORD_DEFAULT);
 
-if(!empty($_POST['changePassword']))
-$change_password = $bdd->prepare('UPDATE user SET pass =:pass WHERE username =:username');
-$change_password->bindParam(':pass', $pass_hash);
-$change_password->bindParam(':username', $_SESSION['username']);
-$change_password->execute();
-header('Location: ../../layouts/settings.php');
+if(!empty($_POST['changePassword']) && $_POST['changePassword'] == $_POST['confirmPassword']){
+    $change_password = $bdd->prepare('UPDATE user SET pass =:pass WHERE username =:username');
+    $change_password->bindParam(':pass', $pass_hash);
+    $change_password->bindParam(':username', $_SESSION['username']);
+    $change_password->execute();
+    header('Location: ../../layouts/settings.php');
+}else{
+    header('Location: ../../layouts/settings.php');
+}
+
 ?>
